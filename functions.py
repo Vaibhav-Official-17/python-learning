fcd433ce9337c04540b2d3c6c3cd5f8ed7a54e93
Vaibhav-Official-17 <vaibhavofficialmail17@gmail.com>
2026-08-17 21:47:41 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,68 @@
+#Functions are the block of code written to solve a problem , rather and writing a peice of code again and again we can define a function and then can use it.
+
+# Greeting Function — Easy
+# def greet():
+#     print("Welcome to python!")
+
+# greet()
+
+#2. Personalized Greeting — Easy
+
+# def greet(name):
+#     print(f"Hello {name}")
+
+# greet("Vaibhav") 
+
+# 3. Add Two Numbers — Easy
+
+# def add(a,b):
+#     return(a+b)
+
+# add=add(12,45)
+# print(add)
+
+# 4. Square Calculator — Easy
+# def square(num):
+#     return(num**2)
+
+# print(square(7))
+
+# Rectangle Calculator — Easy/Medium
+
+# def area(l,b):
+#     return(l*b)
+
+# def perimeter(l,b):
+#     p=2*(l+b)
+#     return p
+
+# print(area(2,2))
+# print(perimeter(2,2))
+
+# 6. Temperature Converter — Medium
+
+# def tempConvertor(c):
+#     f=(c*(9/5))+32
+#     return(f)
+
+# print(tempConvertor(34))
+
+# 7. Age Calculator — Medium
+
+# def ageCalculator(name , age):
+#     return(f"{name} you will {age+1} next year")
+
+# print(ageCalculator("Vaibhav",23))
+
+# 8. Even/Odd Function — Medium
+
+# def numberChecker(num):
+#     if(num%2==0):
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# num=int(input("Enter any number :")) 
+
+# print(numberChecker(num))
+
